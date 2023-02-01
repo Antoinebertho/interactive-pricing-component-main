ad2a79fe303eee311293540c03f6d8904b4c0f00
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-02-01 11:11:19 +0100

    rajout de css

---
+++ index.js
@@ -0,0 +1,8 @@
+let rangeSlider = document.getElementById("slider");
+
+console.log(rangeSlider)
+
+// rangeSlider.oninput = function() {
+//     let value = this.value;
+//     document.getElementById("value").innerHTML = value;
+// }
