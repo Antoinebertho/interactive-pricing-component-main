cbb5eb67793cb27710a3389dda2810a89a8f3622
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-02-01 13:48:42 +0100

    rajout css et style slide

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 let rangeSlider = document.getElementById("slider");
-
-console.log(rangeSlider)
-
-// rangeSlider.oninput = function() {
-//     let value = this.value;
-//     document.getElementById("value").innerHTML = value;
-// }
+let priceRange = document.getElementById("price");
