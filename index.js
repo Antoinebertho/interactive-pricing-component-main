a563362e26ad3ddac176e645a5f5df927d0a4de2
Antoine Bertho <55445208+Antoinebertho@users.noreply.github.com>
2023-02-01 16:43:24 +0100

    changement erreur id money et variable sur doc JS

--- index.js
+++ index.js
@@ -6,37 +6,37 @@ let checkbox = document.getElementById("#switch");
 function checkIfBargain() {
   if (slider.value == 0) {
     if (checkbox.checked) {
-      money.textContent = "$" + (8-(25*8/100));
+      priceRange.textContent = "$" + (8-(25*8/100));
     } else {
-      money.textContent = "$8";
+      priceRange.textContent = "$8";
     }
     pageViews.textContent = "10K PAGEVIEWS";
   } else if (slider.value == 25) {
     if (checkbox.checked) {
-      money.textContent = "$" + (12-(50*12/100));
+      priceRange.textContent = "$" + (12-(50*12/100));
     } else {
-      money.textContent = "$12";
+      priceRange.textContent = "$12";
     }
     pageViews.textContent = "50K PAGEVIEWS";
   } else if (slider.value == 50) {
     if (checkbox.checked) {
-      money.textContent = "$" + (16-(50*16/100));
+      priceRange.textContent = "$" + (16-(50*16/100));
     } else {
-      money.textContent = "$16";
+      priceRange.textContent = "$16";
     }
     pageViews.textContent = "100K PAGEVIEWS";
   } else if (slider.value == 75) {
     if (checkbox.checked) {
-      money.textContent = "$" + (24-(50*24/100));
+      priceRange.textContent = "$" + (24-(50*24/100));
     } else {
-      money.textContent = "$24";
+      priceRange.textContent = "$24";
     }
     pageViews.textContent = "500K PAGEVIEWS";
   } else if (slider.value == 100) {
     if (checkbox.checked) {
-      money.textContent = "$" + (36-(50*36/100));
+      priceRange.textContent = "$" + (36-(50*36/100));
     } else {
-      money.textContent = "$36";
+      priceRange.textContent = "$36";
     }
     pageViews.textContent = "1M PAGEVIEWS";
   }
